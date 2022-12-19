a8ac359482879f693cc6c30f41ee6b188f3053e2
Heber Cordova <hbcordova10@gmail.com>
2022-12-19 11:54:27 -0500

    init: Added project base
    The base project with its dependencies was created

---
+++ src/main/java/com/hbcordova/hymnmusic/HymnMusicApplication.java
@@ -0,0 +1,13 @@
+package com.hbcordova.hymnmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HymnMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HymnMusicApplication.class, args);
+    }
+
+}
